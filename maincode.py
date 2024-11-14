42d1d9465fe394cd28100040323c359cd82c9a86
Kangdahyeon <kdhkdh6345@naver.com>
2024-11-14 23:07:48 +0900

    이미지 조정 및 코드 수정

--- maincode.py
+++ maincode.py
@@ -12,7 +12,27 @@ class Hall:
         self.image.draw(1400 // 2 - camera_x, 800 // 2-camera_y, self.scaled_width, self.scaled_height)
 
 
-class ExtraImage:
+class Image1:
+    def __init__(self):
+        self.image = load_image('start room1.png')
+        self.width = int(1151*1.2)
+        self.height = int(600*1.2)
+
+    def draw(self, camera_x,camera_y):
+        # Hall 이미지 오른쪽에 추가 이미지가 오도록 위치 조정
+        self.image.draw(1400 // 2 - camera_x - 1000, 800 // 2-camera_y+1000, self.width, self.height)
+
+class Image2:
+    def __init__(self):
+        self.image = load_image('secondroom.png')
+        self.width = int(2000)
+        self.height = int(626)
+
+    def draw(self, camera_x,camera_y):
+        # Hall 이미지 오른쪽에 추가 이미지가 오도록 위치 조정
+        self.image.draw(1400 // 2 - camera_x - 3000, 800 // 2-camera_y, self.width, self.height)
+
+class Image3:
     def __init__(self):
         self.image = load_image('start room1.png')
         self.width = int(1151*1.2)
@@ -26,7 +46,10 @@ class ExtraImage:
 def main():
     open_canvas(1400, 800)  # 1400x600 화면 생성
     hall = Hall()  # Hall 객체 생성
-    extra_image = ExtraImage()  # 추가 이미지 객체 생성
+    extra_image1 = Image1()  # 추가 이미지 객체 생성
+    extra_image2 = Image2()  # 추가 이미지 객체 생성
+    extra_image3 = Image3()  # 추가 이미지 객체 생성
+
 
     camera_x = 0  # 카메라 초기 위치
     camera_y=0
@@ -36,7 +59,8 @@ def main():
 
         # Hall과 추가 이미지를 카메라 위치에 맞춰 그리기
         hall.draw(camera_x,camera_y)
-        extra_image.draw(camera_x,camera_y)
+        extra_image1.draw(camera_x,camera_y)
+        extra_image2.draw(camera_x, camera_y)
 
         update_canvas()
 
---
+++ pythonProject/boy.py
@@ -0,0 +1,4 @@
+class Idle:
+    @staticmethod
+    def enter():
+        pass
